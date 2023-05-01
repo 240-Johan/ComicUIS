352d841efae3eefd8bae9f27c47cd3cead209119
alenunez <alejandro2191947@correo.uis.edu.co>
2023-05-01 15:01:12 -0500

    inicio del proyecto en springboot

---
+++ comicUis/src/test/java/com/entornos/comicUis/ComicUisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entornos.comicUis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicUisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
